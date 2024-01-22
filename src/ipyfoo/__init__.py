32744ecbf0608a3c8168b62e2b992f0752f5c8a1
Trevor Manz <trevor.j.manz@gmail.com>
2024-01-22 14:31:22 -0500

    Remove CSS file contents

--- src/ipyfoo/__init__.py
+++ src/ipyfoo/__init__.py
@@ -12,5 +12,4 @@ except importlib.metadata.PackageNotFoundError:
 
 class Counter(anywidget.AnyWidget):
     _esm = pathlib.Path(__file__).parent / "static" / "widget.js"
-    _css = pathlib.Path(__file__).parent / "static" / "widget.css"
     value = traitlets.Int(0).tag(sync=True)
