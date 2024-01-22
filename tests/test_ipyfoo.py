c4f56840798646fdeb673b271cbda3a9f437ae90
Trevor Manz <trevor.j.manz@gmail.com>
2024-01-22 14:13:36 -0500

    Ensure background thread is joined in test

--- tests/test_ipyfoo.py
+++ tests/test_ipyfoo.py
@@ -1,8 +1,10 @@
+from typing import cast
 from ipyfoo import Counter
-
+import anywidget._file_contents
 
 def test_counter():
     counter = Counter()
     counter.value = 10
     assert counter._esm
     assert counter.value == 10
+    cast(anywidget._file_contents.FileContents, Counter._esm)._background_thread.join()
